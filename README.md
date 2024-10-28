065907f913b722d37b9ae754946843a215ad1da0
fernanda <113780367+f3rnn@users.noreply.github.com>
2024-10-28 14:26:05 +0000

    atividade

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# bd-III-senai
+# Atividade
+
+### Objetivos
+Criar um CRUD para um usuário:
+- Create (cirar usuário)
+- Read (listar todos usuários/pesquisar um usuário)
+- Update (atualizar dados de um usuário)
+- Delete (excluir um usuário)
+
+No arquivo principal, crie um menu onde seja possível escolher a ação desejada.
+```
+    === SISTEMA ===
+1 - Adicionar usuário
+2 - Pesquisar um usuário
+3 - Atualizar dados de um usuário
+4 - Excluir um usuário
+5 - Exibir todos os usuário
+0 - Sair
+
+Informe a opção desejada:
+```
+
+Os dados precisam ser salvos no banco de dados.
+
+### Tecnologias:
+- ORM: SQLAlchemy
+- Banco de dados MySQL
+- Containerização: Docker
+- Orquestração de conteiners: Docker Compose
--- app/main.py
+++ app/main.py
@@ -3,23 +3,43 @@ from repositores.usuario_repository import UsuarioRepository
 from config.database import Session
 import os
 
+
 def main():
     session = Session()
     repository = UsuarioRepository(session)
     service = UsuarioService(repository)
 
-    print("\nadicionando usuário:")
-    nome = input("digite seu nome: ")
-    email = input("digite seu e-mail: ")
-    senha = input("digite sua senha: ")
+    while True:
+        print("código\t|\tdescriação")
+        print("1\t|\tcriar usuário")
+        print("2\t|\tpesquisar um usuário")
+        print("3\t|\tatualizar dados de um usuário")
+        print("4\t|\texcluir usuário")
+        print("5\t|\tlistar todos")
+        print("0\t|\tsair")
 
-    service.criar_usuario(nome=nome, email=email, senha=senha)
+        resposta = int(input("informe o código desejado:\n"))
+
+        match(resposta):
+            case 1:
+                print("\nadicionando usuário:")
+                nome = input("digite seu nome: ")
+                email = input("digite seu e-mail: ")
+                senha = input("digite sua senha: ")
+
+                service.criar_usuario(nome=nome, email=email, senha=senha)
+            
+            case 2:
+
 
     print("\nlistando usuários cadastrados")
     lista_usuarios = service.listar_todos_usuarios()
     for usuario in lista_usuarios:
-        print(f"Nome: {usuario.nome} - E-Mail: {usuario.email} - Senha: {usuario.senha}")
-    
+        print(
+            f"Nome: {usuario.nome} - E-Mail: {usuario.email} - Senha: {usuario.senha}"
+        )
+
+
 if __name__ == "__main__":
     os.system("cls || clear")
     main()
